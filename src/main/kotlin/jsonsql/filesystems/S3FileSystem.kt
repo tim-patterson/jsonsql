3d599f5e9e0c12c25d8b5600bd60acb64ccc439a
Tim Patterson <tim.wanaka@gmail.com>
2018-04-29 17:26:29 +1200

    Make sure we don't trucate s3 listing

--- src/main/kotlin/jsonsql/filesystems/S3FileSystem.kt
+++ src/main/kotlin/jsonsql/filesystems/S3FileSystem.kt
@@ -9,8 +9,16 @@ object S3FileSystem: FileSystem {
     private val s3 = AmazonS3ClientBuilder.defaultClient()
 
     override fun listDir(authority: String, path: String): List<String> {
-        val listing = s3.listObjects(authority, path.trimStart('/'))
-        return listing.objectSummaries.filter { it.size > 0 }.map { "$authority/${it.key}" }
+        val results = mutableListOf<String>()
+
+        var listing = s3.listObjects(authority, path.trimStart('/'))
+        results.addAll(listing.objectSummaries.filter { it.size > 0 }.map { "$authority/${it.key}" })
+
+        while(listing.isTruncated) {
+            listing = s3.listNextBatchOfObjects(listing)
+            results.addAll(listing.objectSummaries.filter { it.size > 0 }.map { "$authority/${it.key}" })
+        }
+        return results
     }
 
     override fun open(authority: String, path: String): InputStream {
