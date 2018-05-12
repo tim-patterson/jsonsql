57f37d0a03218796ac95d209843ba2380d5a9dea
Tim Patterson <tim.wanaka@gmail.com>
2018-05-12 18:30:36 +1200

    add basic dir file format

---
+++ src/main/kotlin/jsonsql/fileformats/DirFormat.kt
@@ -0,0 +1,27 @@
+package jsonsql.fileformats
+
+import jsonsql.filesystems.FileSystem
+
+object DirFormat: FileFormat {
+    override fun reader(fs: FileSystem, path: String): FileFormat.Reader = DirReader(fs, path)
+
+    override fun writer(fs: FileSystem, path: String, fields: List<String>): FileFormat.Writer = TODO()
+
+    override fun split(): Boolean = false
+
+
+    private class DirReader(val fs: FileSystem, val path: String): FileFormat.Reader {
+        private val fileIter: Iterator<Map<String, Any?>> by lazy { fs.listDir(path).iterator() }
+
+        override fun next(): Map<String, *>? {
+            return if (fileIter.hasNext()) {
+                fileIter.next()
+            } else {
+                null
+            }
+        }
+
+        override fun close() {}
+    }
+
+}
